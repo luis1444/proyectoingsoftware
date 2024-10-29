b673fb83f96c3d46c53fa17e9ba579e3c676c041
Luis Vega <lvegam3@ucentral.edu.co>
2024-10-29 15:03:39 -0500

    (Modificacion de controladores, Conexion a la base de datos, Los usuarios ya se almacenan en la base de datos)

--- src/main/java/co/ucentral/concesionario/controladores/UsuarioControlador.java
+++ src/main/java/co/ucentral/concesionario/controladores/UsuarioControlador.java
@@ -3,6 +3,10 @@ import co.ucentral.concesionario.persistencia.entidades.Usuario;
 import co.ucentral.concesionario.servicios.UsuarioServicios;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
 
 import java.util.List;
 @AllArgsConstructor
@@ -10,6 +14,23 @@ import java.util.List;
 public class UsuarioControlador {
 
     UsuarioServicios usuarioServicio;
+
+    @GetMapping("/registro")
+    public String mostrarFormularioDeRegistro(Model model) {
+        Usuario usuario = new Usuario();
+
+        model.addAttribute("elusuario", usuario);
+        return "registro";
+    }
+
+    @PostMapping("/registro")
+    public String registrarUsuario(Usuario usuario, Model model) {
+        usuarioServicio.registrarUsuario(usuario);
+        model.addAttribute("mensaje", "Usuario registrado exitosamente");
+        return "iniciosesion";
+
+
+    }
 
     public List<Usuario> obtenerTodos() {
         return usuarioServicio.obtenerTodos();
