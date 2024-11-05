5eb0d58f8c3bfd5524cad816153bf7c9987585a0
Luis Vega <lvegam3@ucentral.edu.co>
2024-11-05 15:17:41 -0500

    (CU09: Creacion de pantalla de fabricante, Conexion de inicio de sesion a pantalla de fabricante por rol)

--- src/main/java/co/ucentral/concesionario/servicios/UsuarioServicios.java
+++ src/main/java/co/ucentral/concesionario/servicios/UsuarioServicios.java
@@ -1,34 +1,43 @@
 package co.ucentral.concesionario.servicios;
+
 import co.ucentral.concesionario.persistencia.entidades.Usuario;
 import co.ucentral.concesionario.persistencia.repositorios.UsuarioRepositorio;
 import org.springframework.beans.factory.annotation.Autowired;
-import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import java.util.List;
 
-@AllArgsConstructor
 @Service
 public class UsuarioServicios {
-    UsuarioRepositorio usuarioRepositorio;
-
 
     @Autowired
-    UsuarioRepositorio UsuarioRepositorio;
+    private UsuarioRepositorio usuarioRepositorio;
+
     public void registrarUsuario(Usuario usuario) {
         usuarioRepositorio.save(usuario);
     }
 
-    public List<Usuario> obtenerTodos(){
-        List<Usuario> listado = (List<Usuario>) usuarioRepositorio.findAll();
-        return listado;
-    }
-    public boolean borrar(Usuario usuario){
-        try{
-            usuarioRepositorio.delete(usuario);
-        }catch (Exception e){
-            return false;
+    public Usuario autenticarUsuario(String nombreUsuario, String contrasena) {
+        Usuario usuarioEncontrado = usuarioRepositorio.findById(nombreUsuario).orElse(null);
+        if (usuarioEncontrado != null) {
+            System.out.println("Usuario encontrado: " + usuarioEncontrado.getUsuario());
+            // Ignorar temporalmente la verificación de la contraseña
+            return usuarioEncontrado;
+        } else {
+            System.out.println("Usuario no encontrado");
         }
-        return true;
+        return null;
     }
 
+    public List<Usuario> obtenerTodos() {
+        return (List<Usuario>) usuarioRepositorio.findAll();
+    }
+
+    public boolean borrar(Usuario usuario) {
+        try {
+            usuarioRepositorio.delete(usuario);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
 }
