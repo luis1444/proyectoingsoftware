4b530e498da45364f47955608f34384d6fa9aa1a
Luis vega <lvegam3@ucentral.edu.co>
2024-11-07 17:17:44 -0500

    (nuevo commit: CU011 Creacion de la funcion modificar vehiculo)

--- src/main/java/co/ucentral/concesionario/servicios/VehiculoServicios.java
+++ src/main/java/co/ucentral/concesionario/servicios/VehiculoServicios.java
@@ -6,12 +6,13 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @AllArgsConstructor
 @Service
 public class VehiculoServicios {
 
-     VehiculoRepositorio vehiculoRepositorio;
+    VehiculoRepositorio vehiculoRepositorio;
 
     public void registrarVehiculo(Vehiculo vehiculo) {
         vehiculoRepositorio.save(vehiculo);
@@ -28,6 +29,29 @@ public class VehiculoServicios {
         } catch (Exception e) {
             e.printStackTrace();
             return false;
+        }
+    }
+    public Vehiculo obtenerPorId(Long id) {
+        Optional<Vehiculo> vehiculo = vehiculoRepositorio.findById(id);
+        return vehiculo.orElse(null);
+    }
+
+    // Método para actualizar un vehículo
+    public void actualizarVehiculo(Vehiculo vehiculo) {
+        Optional<Vehiculo> vehiculoExistente = vehiculoRepositorio.findById(vehiculo.getId());
+        if (vehiculoExistente.isPresent()) {
+            Vehiculo vehiculoActualizado = vehiculoExistente.get();
+            vehiculoActualizado.setMarca(vehiculo.getMarca());
+            vehiculoActualizado.setModelo(vehiculo.getModelo());
+            vehiculoActualizado.setAnio(vehiculo.getAnio());
+            vehiculoActualizado.setColor(vehiculo.getColor());
+            vehiculoActualizado.setPrecio(vehiculo.getPrecio());
+            vehiculoActualizado.setTipoCombustible(vehiculo.getTipoCombustible());
+            vehiculoActualizado.setPaisOrigen(vehiculo.getPaisOrigen());
+            vehiculoActualizado.setFoto(vehiculo.getFoto());
+            vehiculoRepositorio.save(vehiculoActualizado);
+        } else {
+            throw new RuntimeException("Vehículo no encontrado con ID: " + vehiculo.getId());
         }
     }
 }
