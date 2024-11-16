9ba3186e0f482cf64b1c7ef31cef6ac4cb4e1a55
Samuel bustos <you@example.com>
2024-11-16 16:32:59 -0500

    la reserva se guarda y se muestra en la confirmacion de descarga

--- src/main/java/co/ucentral/concesionario/controladores/ClienteControlador.java
+++ src/main/java/co/ucentral/concesionario/controladores/ClienteControlador.java
@@ -1,11 +1,15 @@
 package co.ucentral.concesionario.controladores;
 
+import co.ucentral.concesionario.persistencia.entidades.Cliente;
 import co.ucentral.concesionario.persistencia.entidades.Vehiculo;
+import co.ucentral.concesionario.servicios.ClienteServicios;
 import co.ucentral.concesionario.servicios.VehiculoServicios;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -15,7 +19,18 @@ import java.util.List;
 public class ClienteControlador {
 
     @Autowired
-    private VehiculoServicios vehiculoServicios;
+    private ClienteServicios clienteServicios;
 
+    @PostMapping("/registrar")
+    public String registrarReserva(@ModelAttribute Cliente cliente, Model model) {
+        Cliente clienteGuardado = clienteServicios.registrarReserva(cliente);
+        model.addAttribute("cliente", clienteGuardado);
+        return "redirect:reservaConfirmadescargar.html  " + clienteGuardado.getId();
+    }
+    @GetMapping
+    public String listarClientes(Model model) {
+        model.addAttribute("clientes", clienteServicios.obtenerTodosLosClientes());
+        return "listaClientes"; // Vista para mostrar la lista de clientes
+    }
 
 }
